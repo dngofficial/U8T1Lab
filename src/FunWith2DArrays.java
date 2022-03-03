094ded203e1921fa17d0655b59e2a72f040b9f8c
BT_1E10_17 <sample@bths.edu>
2022-03-03 13:34:11 -0500

    more solutions added

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -22,5 +22,46 @@ public class FunWith2DArrays
         System.out.println(strArray[strArray.length - 1][strArray[0].length  -1]);
 
     }
+
+    public static double average(int[][] intArray)
+    {
+        double sum = 0;
+        int total = 0;
+        for (int[] nums : intArray)
+        {
+            for (int nums1 : nums)
+            {
+                sum += nums1;
+                total++;
+            }
+        }
+
+        return sum / total;
+    }
+
+    public static int[] indexFound(String[][] strArray, String searchTerm)
+    {
+
+
+        int[] finalArray = new int[2];
+        for (int i = 0; i < strArray.length; i++)
+        {
+            for (int j = 0; j < strArray[0].length; j++)
+            {
+
+                if (strArray[i][j].equals(searchTerm))
+                {
+                    finalArray[0] = i;
+                    finalArray[1] = j;
+                    return finalArray;
+                }
+            }
+
+        }
+        finalArray[0] = -1;
+        finalArray[1] = -1;
+
+        return finalArray;
+    }
 }
 
